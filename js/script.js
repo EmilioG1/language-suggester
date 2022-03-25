033f1fbe756ca9c826cf5ba6ff5699b8e3f9166a
Emilio Gonzales <emiliog1184@gmail.com>
2022-03-25 10:43:41 -0700

    Add branching to script file

--- js/script.js
+++ js/script.js
@@ -7,14 +7,39 @@
 $(document).ready(function() {
   $("form#language").submit(function(event) {
     event.preventDefault();
-    const front = $("input#front").val();
-    const back = $("input#back").val();
-    const middle = $("input#middle").val();
+    const plane = $("input:radio[name=plane]:checked").val();
+    // const back = $("input#back").val();
+    // const middle = $("input#middle").val();
     const bev = $("select#drink").val();
     const travelYes = $("input#yes").val()
-    const travelNo = $("input#no").no();
-    const num = $("input#num").val()
+    const travelNo = $("input#no").val();
+    const num = parseInt($("input#num").val());
 
+
+
+
+    if (num <= 4) {
+      if (bev === "1") {
+        $("#ruby").show();
+      } else if (bev === "2") {
+        $("#python").show();
+      } else {
+        $("#trouble").show();
+      }
+    } else if (num >= 5 && num <= 10) {
+      if (plane === "front") {
+        $("#javascript").show();
+      } else if (plane === "back") {
+        $("#python").show();
+      } else if (plane === "middle") {
+        $("#ruby").show();
+      } else {
+        $("#trouble").show();
+      }
+    } else {
+      $("#trouble").show();
+    }
+  
 
   });
 });
